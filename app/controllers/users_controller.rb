f8c0022c6c1922b647029dc6207db832a8072430
htylerbrooks <htb5qh@gmail.com>
2015-04-21 17:14:16 -0400

    getting rid of development log untracked files so I can pull

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new( user_params )
         if @user.save
-            redirect_to city_homeless_person_path(@user.id)
+            redirect_to city_homeless_person_path(@user)
         else
             render :new
         end
