dd95167f411f7b0de1c149200eefac5e6d4b678d
Chad Smith <chadxsmith@ymail.com>
2015-04-21 14:16:30 -0400

    chad's stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418182459) do
+ActiveRecord::Schema.define(version: 20150420234808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "homeless_pop"
+    t.text     "img"
+    t.text     "homeless_bio"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "description_title"
+    t.string "description_text"
+    t.string "description_code"
+  end
 
 end
