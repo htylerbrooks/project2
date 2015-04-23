53cb38a05dd3eea70ae5c90efd69d136c6aedb14
Brad Foster <btf5048@gmail.com>
2015-04-23 09:51:44 -0400

    trouble shooting bad uri heroku error

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,8 +7,7 @@ class CitiesController < ApplicationController
     @user = User.new
     @city = City.find(params[:id])
     @homeless = @city.homeless_people
-    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=#{@city.name}")
-    # @weather_description = response["weather"]
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=chicago")
     @kelvin_temperature = response["main"]["temp"]
     @fahrenheit_temperature = ((@kelvin_temperature - 273.15)*1.8) + 32
     @temperature = @fahrenheit_temperature.round.to_s + "° F"
